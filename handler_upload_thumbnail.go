d2ccf8b692144b68fa6c195ce60a4af62e6dec04
tfry <roswall.tim@gmail.com>
2025-12-11 11:42:05 +0900

    test base64 encoding

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
+	"io"
+	"log"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -28,10 +31,50 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
+	const maxMemory = 10 << 20
+	if err := r.ParseMultipartForm(maxMemory); err != nil {
+		log.Printf("ParseMultipartForm returned err: %v", err)
+	}
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	data, headers, err := r.FormFile("thumbnail")
+	if err != nil {
+		log.Printf("FormFile returned err: %v", err)
+	}
+	mediaType := headers.Header.Get("Content-Type")
+	dataSlice, err := io.ReadAll(data)
+	if err != nil {
+		log.Printf("io.ReadAll returned err: %v", err)
+	}
+
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		log.Printf("GetVideo returned err: %v", err)
+	}
+	if userID != video.UserID {
+		respondWithError(w, http.StatusUnauthorized, "unauthorized", nil)
+		fmt.Printf("expected: %v\ngot: %v\n", userID, video.UserID)
+		return
+	}
+	thumbnail := thumbnail{
+		data:      dataSlice,
+		mediaType: mediaType,
+	}
+	base64Data := base64.StdEncoding.EncodeToString(thumbnail.data)
+
+	// videoThumbnails[video.ID] = thumbnail
+
+	// fmt.Printf("PORT: %v\n", cfg.port)
+	// thumbnailURL := fmt.Sprintf("http://localhost:%v/api/thumbnails/%v", cfg.port, video.ID)
+	dataURL := fmt.Sprintf("data:%v;base64,%v", thumbnail.mediaType, base64Data)
+
+	video.ThumbnailURL = &dataURL
+	if err := cfg.db.UpdateVideo(video); err != nil {
+		fmt.Printf("UpdateVideo returned err: %v", err)
+	}
+
+	// returnVideo := database.Video(video)
+	respondWithJSON(w, http.StatusOK, video)
 }
